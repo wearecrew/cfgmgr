cb3f18e8ffc299248fedeba13f52bf79fed204bf
Simon Cam <heyscam@gmail.com>
2021-10-23 09:53:34 +0100

    Fix py_modules

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="secrets",
     version="0.1",
-    py_modules=["core"],
+    py_modules=["main"],
     install_requires=[
         "Click",
         "cryptography",
